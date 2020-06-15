140ea59c54a3aebb250014d7f13c48c0499f20f0
Carolene Wilson <carolenesw@gmail.com>
2020-06-15 16:48:50 -0400

    add table rows on click event and color

--- designs.js
+++ designs.js
@@ -1,25 +1,26 @@
-
 // Select size input
 const sizeInput = document.getElementById("sizePicker");
 let inputWEl = ""; 
 let inputHEl = "";
 
 // Select color input
-const inputCol = document.getElementById("colorPicker");
+let inputCol = "";
 const pixelCan = document.getElementById("pixelCanvas");
 
-// create table row to append to pixel Convas 
-const tableRows = document.createElement("tr");
+// create table row and data to append to pixel Convas 
+let tableRows = document.createElement("tr");
+const tData = document.createElement("td")
 
-// const trId = document.createElement("")
 
 // When size is submitted by the user, call makeGrid()
 sizeInput.addEventListener("submit", function() {
 
 event.preventDefault()
 
-inputHEl = document.getElementById("inputWidth").value;
-inputWEl = document.getElementById("inputHeight").value;
+// add click events with values
+inputHEl = document.getElementById("inputHeight").value;
+inputWEl = document.getElementById("inputWidth").value;
+inputCol = document.getElementById("colorPicker").value;
 
 makeGrid(inputHEl, inputWEl)
 
@@ -29,16 +30,39 @@ console.log("Height: " + inputHEl + " and Width: " + inputWEl)
 
     //validation code to see State field is mandatory.  
 
-
 function makeGrid(height, width) {
-// prevent duplications when page is refresh 
+// prevent duplications when page is refresh and add table rows 
 tableRows.style.display = "none"
+tableRows = pixelCan.insertRow()
 
 for (var i = 1; i <= height; i++) {
     // create id for each increment for the table row
-    pixelCan.append(tableRows.setAttribute("id", "trTable")+ i)
-   console.log(tableRows)
+    // pixelCan.append(tableRows.setAttribute("id", "trTable" + i) + i)
+// document.write("what is the number" + i)
+    // pixelCan.append(tableRows.setAttribute("id", "trTable")+ i) 
+    // pixelCan = pixelCan + i
+    // tableRows.setAttribute("id", "trTable")
+    
+    // pixelCan.append(tableRows)
+    // tableRows.setAttribute("id", "trTable")
+    // tableRows.value
+    // trTable.value
+// console.log(trTable + i)
+    // for (var x = 1; x <= width; x++) {
+
+    //     // add data for each row
+    //     tableRows.append("tData") + i
+    //     console.log(x)
+    //     // console.log(trID)
+    // }
+   console.log(tableRows + i)
+   console.log(pixelCan)
+   console.log(i)
 }
 // Your code goes here!
 
+}
+
+for (var i = 0; i< 10; i++) {
+    document.write(" my number is " + i );
 }
